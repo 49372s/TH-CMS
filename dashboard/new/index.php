fb09d7ff61044f939e0c4c534be8abf3921745b8
49372s <kyouyouidsiyou@gmail.com>
2026-08-25 14:52:47 +0900

    feat: visual editor

--- dashboard/new/index.php
+++ dashboard/new/index.php
@@ -34,9 +34,9 @@ loginRedirect();
                     <label class="form-check-label" for="autoPost">自動投稿(Misskey)</label><br>
                 </div>
                 <hr>
-                <div class="mb-3 form-floating">
-                    <textarea name="body" id="article-body" class="form-control" placeholder="leave a comment." style="height: 30vh;"></textarea>
-                    <label for="article-body">本文</label>
+                <div class="mb-3">
+                    <label for="article-body" class="form-label">本文</label>
+                    <textarea name="body" id="article-body" class="form-control" placeholder="記事の本文を入力してください" style="height: 30vh;"></textarea>
                     <p>画像をアップロードしたい場合は<a href="imageUpload.php" target="_blank" rel="noopener noreferrer">こちら</a></p>
                 </div>
                 <button type="submit" class="btn btn-primary">投稿する</button>
@@ -47,6 +47,27 @@ loginRedirect();
                 <span class="visually-hidden">Loading...</span>
             </div>
         </div>
+        <script src="https://cdn.jsdelivr.net/npm/tinymce@6/tinymce.min.js" referrerpolicy="origin"></script>
+        <script>
+            tinymce.init({
+                selector: '#article-body',
+                language: 'ja',
+                height: 500,
+                menubar: true,
+                plugins: [
+                    'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+                    'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+                    'insertdatetime', 'media', 'table', 'help', 'wordcount'
+                ],
+                toolbar: 'undo redo | blocks | ' +
+                    'bold italic underline strikethrough | forecolor backcolor | alignleft aligncenter ' +
+                    'alignright alignjustify | bullist numlist outdent indent | ' +
+                    'removeformat | link image media table | code fullscreen | help',
+                content_style: 'body { font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; font-size: 14px }',
+                promotion: false,
+                branding: false
+            });
+        </script>
         <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
         <script src="/includes/script/edit.js?<?=time()?>"></script>
